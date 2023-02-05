98c8b12879209c0197e626590e0359b6743cb0db
bmauncho <84400423+bmauncho@users.noreply.github.com>
2023-02-05 20:06:43 +0300

    Changes

--- Snakes Vs Fruits/Assets/Scripts/SnakeMovement.cs
+++ Snakes Vs Fruits/Assets/Scripts/SnakeMovement.cs
@@ -14,6 +14,9 @@ public class SnakeMovement : MonoBehaviour
     public int gap = 10;
     public float BodySpeed = 5f;
     public Transform parent;
+    [SerializeField] private FloatingJoystick _joystick;
+    Vector3 _moveVector;
+    public bool IsMoving = true;
 
     // Start is called before the first frame update
     void Start()
@@ -29,23 +32,34 @@ public class SnakeMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Move();
+    }
+
+    private void Move()
+    {
+        _moveVector = Vector3.zero;
+        _moveVector.x = _joystick.Horizontal;
+
+        if(_joystick.Horizontal!=0)
+        {
+            float SteerDir = _joystick.Horizontal;
+            transform.Rotate(Vector3.up * SteerDir * steerSpeed * Time.deltaTime);
+        }
+
         transform.position += transform.forward * moveSpeed * Time.deltaTime;
-
-        float SteerDir = Input.GetAxis("Horizontal");
-        transform.Rotate(Vector3.up * SteerDir * steerSpeed * Time.deltaTime);
-
         Pos_History.Insert(0,transform.position);
 
-        int index = 0;
-        foreach(var body in BodyParts)
-        {
-            Vector3 point = Pos_History[Mathf.Min(index * gap,Pos_History.Count-1)];
-            Vector3 move_Dir = point - body.transform.position;
-            Vector3 smoothMove = Vector3.Lerp(body.transform.position,move_Dir,5f*Time.deltaTime);
-            body.transform.position += move_Dir * BodySpeed * Time.deltaTime;
-            body.transform.LookAt(point);
-            index++;
-        }
+            int index = 0;
+            foreach(var body in BodyParts)
+            {
+                Vector3 point = Pos_History[Mathf.Min(index * gap,Pos_History.Count-1)];
+                Vector3 move_Dir = point - body.transform.position;
+                Vector3 smoothMove = Vector3.Lerp(body.transform.position,move_Dir,5f*Time.deltaTime);
+                body.transform.position += move_Dir * BodySpeed * Time.deltaTime;
+                body.transform.LookAt(point);
+                index++;
+            }
+        
     }
 
     public void GrowSnake()
